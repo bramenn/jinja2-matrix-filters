4e4890897382410a423a892df4e27d4e5032e0bb
Alejandro Herrera <bramendev@gmail.com>
2023-04-04 14:43:52 -0500

    Added more documentation and the LICENSE

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name="jinja2_matrix_filters",
+    name="jinja2-matrix-filters",
     version="0.1.0",
-    url="https://github.com/bramenn/jinja2_matrix_filters",
+    url="https://github.com/bramenn/jinja2-matrix-filters",
     author="Alejandro Herrera",
     author_email="bramendev@gmail.com",
     description="A Jinja plugin for handling Matrix data",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
 )
